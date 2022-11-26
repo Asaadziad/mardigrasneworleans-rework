bc5e8fd1cd2766f2adf5c7ec856d5411a5b10a10
Asaadziad <asaadabuelhija@gmail.com>
2022-11-26 07:08:04 +0200

    updated UI

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const loadingBtns = document.querySelectorAll(".loadedItem");
+
+loadingBtns.forEach((item) =>
+  item.addEventListener("click", function () {
+    loadingBtns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    item.classList.add("active");
+  })
+);
 function loadNav() {
   const navItems = ["Home", "About", "LOGO HERE", "Media", "Contact"];
   const nav = document.getElementById("nav-items");
@@ -17,6 +27,44 @@ function loadNav() {
   }
 }
 
-function loadCarousel() {}
+function loadSponsors(firstIndex, lastIndex) {
+  const sponsorsList = [
+    "Walk-On's Bistreaux and Bar",
+    "Sheraton New Orleans Hotel",
+    " Ace Hotel New Orleans",
+    "The Ritz-Carlton, New Orleans",
+    " Premium Parking - P402",
+    "Louisiana Children's Museum",
+    "Museum of Death",
+    "Haunted Museum",
+  ];
+  const sponsors = document.getElementById("sponsors");
+  if (sponsors) {
+    for (let i = firstIndex; i <= lastIndex; i++) {
+      sponsors.innerHTML +=
+        `<div class="card sponsorCard" style="width: 18rem;height:18rem">
+    <img src="..." class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">` +
+        sponsorsList[i] +
+        `</h5>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the
+        bulk of the card's content.
+      </p>
+      <a href="#" class="btn btn-primary">Read more</a>
+    </div>
+  </div>`;
+    }
+  }
+}
 
-loadNav();
+function loadFooter() {
+  const footer = document.getElementById("footer");
+  footer.innerHTML += '<div class="container">test</div>';
+  footer.innerHTML += '<div class="container">test</div>';
+}
+
+//loadNav();
+loadSponsors(2, 5);
+loadFooter();
